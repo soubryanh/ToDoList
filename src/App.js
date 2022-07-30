afb962c7ee4358b07f2a548b52a03d6fc5863ee1
bryan <bryanhernandz102@gmail.com>
2022-07-30 16:12:06 -0300

    add Check and delete ToDos functions

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ const allDefault = [
 ];
 
 function App() {
-  const [toDos, setToDos] = React.useState(allDefault); // this is a state
+  const [toDos, setToDos] = React.useState(allDefault); // this is a state, array
   const [searchValue, setSearchValue] = React.useState("");
   const completedToDos = toDos.filter((toDo) => !!toDo.completed).length; // !! is double negative that means, positive/true
+  // aquí se busca la cantidad de toDos marcados como completados.
   const totalToDos = toDos.length;
-
   let searchedToDos = [];
 
   if (!searchValue.length >= 1) {
@@ -28,6 +28,25 @@ function App() {
       return toDoText.includes(searchText);
     });
   }
+  // aquí filtramos los toDos para encontrar el que corresponda con lo que escriba el user
+
+  const completeToDo = (text) => {
+    const toDoIndex = toDos.findIndex((toDo) => toDo.text === text);
+    // lo que está dentro de la función sirve como condicional para comparar dos valores.
+    //por cada toDo vamos a ver si nuestro toDo.text incluye esos parámetros.
+    const newToDos = [...toDos];
+    // newToDos[toDoIndex].completed = true;
+    newToDos[toDoIndex].completed = !newToDos[toDoIndex].completed;
+    setToDos(newToDos);
+  };
+
+  //Función Busca el item que coinside con el mismo texto entre los arrays y cambia el estado a completado para subrayar la tarea.
+
+  const deleteToDo = (text) => {
+    const newToDos = toDos.filter((toDo) => toDo.text !== text);
+    setToDos(newToDos);
+  };
+  // Usar el método split to delete an item directly in the array 'newToDos', sin entrar al index
 
   return (
     // Components
@@ -37,7 +56,13 @@ function App() {
 
       <ToDoList>
         {searchedToDos.map((all) => (
-          <ToDoItem key={all.text} text={all.text} completed={all.completed} />
+          <ToDoItem
+            key={all.text}
+            text={all.text}
+            completed={all.completed}
+            onComplete={() => completeToDo(all.text)}
+            onDelete={() => deleteToDo(all.text)}
+          />
         ))}
       </ToDoList>
 
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -2,34 +2,22 @@ import React from "react";
 import "./ToDoItem.css";
 
 function ToDoItem(props) {
-  const onComplete = () => {
-    alert("Ya completaste el todo " + props.text);
-  };
-  const onDelete = () => {
-    alert("Valeu " + props.text);
-  };
   return (
-    <section>
-      <li className="ToDoItem">
-        <span
-          className={`Icon Icon-check ${
-            props.completed && "Icon-check--active"
-          }`}
-          onClick={onComplete}
-        >
-          C
-        </span>
-        <p
-          className={`ToDoItem-p ${props.completed && "ToDoItem-p--complete"}`}
-        >
-          {/* "${}" Se renombra la clase si recibe la propiedad complete, hace que se subraye*/}
-          {props.text}
-        </p>
-        <span className="Icon Icon-delete" onClick={onDelete}>
-          X
-        </span>
-      </li>
-    </section>
+    <li className="ToDoItem">
+      <span
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}
+      >
+        C
+      </span>
+      <p className={`ToDoItem-p ${props.completed && "ToDoItem-p--complete"}`}>
+        {/* "${}" Se renombra la clase si recibe la propiedad complete, hace que se subraye*/}
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
+        X
+      </span>
+    </li>
   );
 }
 
